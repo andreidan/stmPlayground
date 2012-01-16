c57bde0b94feaa3124ea7727fcb464fdb87d17af
Andrei Dan <adan@smallfootprint.com>
2012-01-16 23:14:47 +0200

    Make Product trully immutable.

--- src/java/com/stm/playground/model/Product.java
+++ src/java/com/stm/playground/model/Product.java
@@ -10,20 +10,20 @@ import akka.stm.Ref;
  * 
  */
 @SuppressWarnings("unused")
-public class Product {
+public final class Product {
 
 	public final UUID id;
 	public final String name;
 	public final String description;
-	private final Integer price;
-	private Ref<Integer> availableInStockCount;
+	private final Ref<Integer> price;
+	private final Ref<Integer> availableInStockCount;
 
 	public Product(String name, String description, Integer price) {
 		super();
 		id = UUID.randomUUID();
 		this.name = name;
 		this.description = description;
-		this.price = price;
+		this.price = new Ref<Integer>(price);
 		// 0 in stock by default
 		this.availableInStockCount = new Ref<Integer>(0);
 	}
